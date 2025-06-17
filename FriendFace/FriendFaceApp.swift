6eac9ece565e872504efb604019e7ad6419c6053
Chiraphat Techasiri <ctechasiri@gmail.com>
2025-06-16 21:18:16 -0700

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Chiraphat Techasiri on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
